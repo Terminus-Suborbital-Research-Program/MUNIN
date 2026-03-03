05bff2f70d6d5951d5c519e8fb75f58629a0f4e5
LoganRilling <loganrilling@gmail.com>
2026-03-02 21:12:00 -0600

    moved system clear

--- main.cpp
+++ main.cpp
@@ -72,10 +72,10 @@ int main()
 
         revs += ( (microsteps % constants::MICROSTEPS_PER_REV) == 0 ) ? 1 : 0;
 
+        system("clear");
         //Track revolutions when a new revolution has finished
         if (( (microsteps % constants::MICROSTEPS_PER_REV) == 0 ) ) { std::cout << "Revolutions: " << revs << std::endl; }
         std::cout << microsteps << std::endl;
-        system("clear");
 
         //Exit conditions(s)
         exit = revs >= 40;
