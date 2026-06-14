0a05061fb1c5cc83113eec2cbda5c41ba3627449
LoganRilling <loganrilling@gmail.com>
2026-06-14 13:40:16 -0500

    Testing RP: Swapped pins

--- pointing/src/constants.hpp
+++ pointing/src/constants.hpp
@@ -10,10 +10,10 @@ namespace constants
 
     const unsigned int MICROSTEPS_PER_REV = 200;
 
-    const unsigned int AZIMUTH_STEP_PIN = 11; //GPIO 17
+    const unsigned int AZIMUTH_STEP_PIN = 12; //GPIO 18
     const unsigned int AZIMUTH_DIR_PIN = 16; //GPIO 23
 
-    const unsigned int ELEVATION_STEP_PIN = 12; //GPIO 18
+    const unsigned int ELEVATION_STEP_PIN = 11; //GPIO 17
     const unsigned int ELEVATION_DIR_PIN = 15; //GPIO 22
 
     const std::chrono::microseconds INIT_PWM_DELAY = std::chrono::microseconds(50);
