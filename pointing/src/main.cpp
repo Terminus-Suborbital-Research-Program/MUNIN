e523645e44c0a8a1b05a85009f718c9ec565cd0d
LoganRilling <loganrilling@gmail.com>
2026-06-14 12:38:33 -0500

    Deleting constants::SOCKET_PATH at the very end of main

--- pointing/src/main.cpp
+++ pointing/src/main.cpp
@@ -44,6 +44,8 @@ int main()
         azimuth_motor.drive();
         //std::cout << "\nDrive function completed\n\r";
     }
+
+    delete constants::SOCKET_PATH;
 }
 
 void MoveData::readData(std::string input_data)
