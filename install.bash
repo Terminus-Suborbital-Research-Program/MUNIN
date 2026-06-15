3d6d9399e5197ad08fd6263e65f7e09d56511504
Tonez3D <tonez3d@gmail.com>
2026-06-14 20:58:16 -0500

    feat: service ini fix: `install.bash` debugging

--- install.bash
+++ install.bash
@@ -5,12 +5,12 @@ bin=/usr/local/bin/munin
 git checkout release && git pull
 
 cd ./pointing
-make .
+make 
 
-mkdir $bin
-cp -r ./output/build $bin/pointing
+mkdir -p $bin
+cp ./outputs/main $bin/pointing
 
 cd ../
 cp -r ./tracking $bin/tracking
 
-
+cp munin-auto-start.service /etc/systemd/system/munin-auto-start.service
