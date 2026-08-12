cdd055ed88bfafc650d3b1210631be9d0362462c
LoganRilling <loganrilling@gmail.com>
2026-08-12 19:00:46 -0400

    Removed bmm350 and bmi323 init

--- pointing/src/main.cpp
+++ pointing/src/main.cpp
@@ -26,22 +26,30 @@ void calibrateElevation(Motor& elevation_motor, MoveData data);
 
 int main()
 {
-    bmi3_sensor_data imu_data[2] = { 0 };
-    imu_data[0].type = BMI323_ACCEL;
-    imu_data[1].type = BMI323_GYRO;
+    // bmi3_sensor_data imu_data[2] = { 0 };
+    // imu_data[0].type = BMI323_ACCEL;
+    // imu_data[1].type = BMI323_GYRO;
 
-    bmm350_mag_temp_data mag_data[3] = { 0 };
+    // bmm350_mag_temp_data mag_data[3] = { 0 };
 
-    bmm350_dev mag = { 0 };
-    bmi3_dev imu = { 0 };
+    // bmm350_dev mag = { 0 };
+    // bmi3_dev imu = { 0 };
 
-    MoveData data;
+    // MoveData data;
     //To get data sensor data to sensor_data: bmi323_get_sensor_data(sensor_data, 3, &dev);
 
     Motor azimuth_motor(constants::MICROSTEPS_PER_REV, constants::AZIMUTH_STEP_PIN, constants::AZIMUTH_DIR_PIN, constants::INIT_PWM_DELAY, constants::GPIO_CONTROLLER_PATH);
     Motor elevation_motor(constants::MICROSTEPS_PER_REV, constants::ELEVATION_STEP_PIN, constants::ELEVATION_DIR_PIN, constants::INIT_PWM_DELAY, constants::GPIO_CONTROLLER_PATH);
 
-    init(mag, imu, azimuth_motor, elevation_motor);
+    //init(mag, imu, azimuth_motor, elevation_motor);
+
+    azimuth_motor.setPID(1, 0, 0, 2 * constants::INIT_PWM_DELAY);
+    azimuth_motor.setSetpointType(Motor::SetpointType::kSTEP);
+    azimuth_motor.usePID(true);
+
+    elevation_motor.setPID(1, 0, 0, 2 * constants::INIT_PWM_DELAY);
+    elevation_motor.setSetpointType(Motor::SetpointType::kSTEP);
+    elevation_motor.usePID(true);
 
     azimuth_motor.setPID(0.5, 0.0, 0.5, constants::INIT_PWM_DELAY);
     azimuth_motor.setStepSetpoint(400);
