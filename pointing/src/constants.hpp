0be559648bf762d52a6749bc901e53e6c8050701
LoganRilling <loganrilling@gmail.com>
2026-08-12 19:26:02 -0400

    Changed motor pin constants

--- pointing/src/constants.hpp
+++ pointing/src/constants.hpp
@@ -12,11 +12,11 @@ namespace constants
 
     const unsigned int MICROSTEPS_PER_REV = 200;
 
-    const unsigned int ELEVATION_STEP_PIN = 27;
-    const unsigned int ELEVATION_DIR_PIN = 23; 
+    const unsigned int ELEVATION_STEP_PIN = 13;
+    const unsigned int ELEVATION_DIR_PIN = 16; 
 
-    const unsigned int AZIMUTH_STEP_PIN = 17;
-    const unsigned int AZIMUTH_DIR_PIN = 22; 
+    const unsigned int AZIMUTH_STEP_PIN = 11;
+    const unsigned int AZIMUTH_DIR_PIN = 15; 
 
     const std::chrono::microseconds INIT_PWM_DELAY = std::chrono::microseconds(1000);
 }
